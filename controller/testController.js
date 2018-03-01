8b417f6bbbf8a8ea46da7916f3bfece0213df458
Abdul Khoir <abdul.khoir@rumah123.com>
2018-03-01 13:23:48 +0700

    fix with eslint babel

--- controller/testController.js
+++ controller/testController.js
@@ -1,17 +1,17 @@
 import testService from 'services/testService'
 
 export const promiseAsync = async (req, res, next) => {
-  let test = await testService.asyncAwait();
-  console.log(test);
-  let test1 = testService.promise();
-  console.log(test1);
+  let test = await testService.asyncAwait()
+  console.log(test)
+  let test1 = testService.promise()
+  console.log(test1)
   res.send(test)
 }
 
 export const IIFE = async (req, res, next) => {
-  let oke = ((bisa)=> {
-    console.log("oke");
-  })(this);
+  let oke = ((bisa) => {
+    console.log('oke')
+  })(this)
   console.log(oke)
-  res.send({IIFE:true})
+  res.send({IIFE: true})
 }
--- services/testService.js
+++ services/testService.js
@@ -1,46 +1,46 @@
-import { setTimeout } from "timers";
-import { resolve } from "path";
+import { setTimeout } from 'timers'
+import { resolve } from 'path'
 
 class TestService {
-    constructor(){
+  constructor () {
 
-    }
+  }
 
-    getlisting1 () {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                console.log("dapet 1")
-                resolve(3)
-            }, 5000);
-        });
-    }
+  getlisting1 () {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log('dapet 1')
+        resolve(3)
+      }, 5000)
+    })
+  }
 
-    getlisting2 (id) {
-        return new Promise((res) => {
-            setTimeout(()=> {
-                console.log("dapet 2")
-                res({
-                    number: id
-                })
-            }, 2000)
+  getlisting2 (id) {
+    return new Promise((res) => {
+      setTimeout(() => {
+        console.log('dapet 2')
+        res({
+          number: id
         })
-    }
-    
-    async asyncAwait(){
-        let oke1 = await this.getlisting1();
-        let oke2 = await this.getlisting2(oke1);
-        return oke2;
-    }
+      }, 2000)
+    })
+  }
 
-    promise(){
-        return new Promise((resolve) => {
-            this.getlisting1().then((result) => {
-                this.getlisting2(result).then((res)=>{
-                    resolve(res);
-                })
-            })
+  async asyncAwait () {
+    let oke1 = await this.getlisting1()
+    let oke2 = await this.getlisting2(oke1)
+    return oke2
+  }
+
+  promise () {
+    return new Promise((resolve) => {
+      this.getlisting1().then((result) => {
+        this.getlisting2(result).then((res) => {
+          resolve(res)
         })
-    }
+      })
+    })
+  }
 }
 
-export default new TestService();
+export default new TestService()
